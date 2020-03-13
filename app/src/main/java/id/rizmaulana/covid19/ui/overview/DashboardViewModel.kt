01e11df93a0e7d197adcc8fcfba87a951118df5f
Pandu <pandu.baraja@airyrooms.com>
2020-03-13 10:05:48 +0700

    Refactor: Combine API Call for Overview and Daily with combineLatest.

--- app/src/main/java/id/rizmaulana/covid19/ui/overview/DashboardViewModel.kt
+++ app/src/main/java/id/rizmaulana/covid19/ui/overview/DashboardViewModel.kt
@@ -5,11 +5,17 @@ import androidx.lifecycle.MutableLiveData
 import id.rizmaulana.covid19.data.model.CovidDaily
 import id.rizmaulana.covid19.data.model.CovidOverview
 import id.rizmaulana.covid19.data.repository.Repository
+import id.rizmaulana.covid19.ui.adapter.viewholders.DailyItem
+import id.rizmaulana.covid19.ui.adapter.viewholders.OverviewItem
+import id.rizmaulana.covid19.ui.adapter.viewholders.TextItem
+import id.rizmaulana.covid19.ui.base.BaseViewItem
 import id.rizmaulana.covid19.ui.base.BaseViewModel
 import id.rizmaulana.covid19.util.Constant
 import id.rizmaulana.covid19.util.SingleLiveEvent
 import id.rizmaulana.covid19.util.ext.addTo
 import id.rizmaulana.covid19.util.rx.SchedulerProvider
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 
 /**
  * rizmaulana
@@ -27,47 +33,62 @@ class DashboardViewModel(
     val errorMessage: LiveData<String>
         get() = _errorMessage
 
-    private val _dailyListData = MutableLiveData<List<CovidDaily>>()
-    val dailyListData: LiveData<List<CovidDaily>>
-        get() = _dailyListData
+    private val _liveItems = MutableLiveData<List<BaseViewItem>>()
+    val items: LiveData<List<BaseViewItem>>
+        get() = _liveItems
 
-    private val _overviewData = MutableLiveData<CovidOverview>()
-    val overviewData: LiveData<CovidOverview>
-        get() = _overviewData
+    fun loadOverviewAndDaily() {
+        val overviewObservable = appRepository.overview()
+            .subscribeOn(schedulerProvider.io())
+            .observeOn(schedulerProvider.io()) //all stream below will be manage on io thread
+            .map { mapOverviewResponse(it) }
 
-    fun getOverview() {
-        appRepository.overview()
-            .observeOn(schedulerProvider.ui())
-            .doOnSubscribe {
-                val cache = appRepository.getCacheOverview()
-                if (cache == null) _loading.postValue(true) else {
-                    _overviewData.postValue(cache)
-                }
-            }
-            .doFinally { _loading.postValue(false) }
-            .subscribe({
-                _overviewData.postValue(it)
-            }, {
-                _errorMessage.postValue(Constant.ERROR_MESSAGE)
-            })
-            .addTo(compositeDisposable)
+        val dailyObservable = appRepository.daily()
+            .subscribeOn(schedulerProvider.io())
+            .observeOn(schedulerProvider.io()) //all stream below will be manage on io thread
+            .map { mapDailyResponse(it) }
+
+        Observable.combineLatest(overviewObservable, dailyObservable, BiFunction<OverviewItem?, List<BaseViewItem>?, List<BaseViewItem>> { overview, daily ->
+            val items: MutableList<BaseViewItem> = mutableListOf()
+
+            //GENERATE SCREEN POSITION HERE
+            items.add(overview)
+            items.add(TextItem("Daily Updates"))
+            items.addAll(daily)
+
+            return@BiFunction items.toList()
+        })
+        .observeOn(schedulerProvider.ui()) //go back to ui thread
+        .subscribe({
+            _liveItems.postValue(it)
+        }, {
+            _errorMessage.postValue(Constant.ERROR_MESSAGE)
+        }).addTo(compositeDisposable)
     }
 
-    fun getDailyUpdate() {
-        appRepository.daily()
-            .observeOn(schedulerProvider.ui())
-            .doOnSubscribe {
-                appRepository.getCacheDaily()?.let { data ->
-                    _dailyListData.postValue(data)
-                }
-            }
-            .subscribe({
-                _dailyListData.postValue(it)
-            }, {
-                _errorMessage.postValue(Constant.ERROR_MESSAGE)
-            })
-            .addTo(compositeDisposable)
+    private fun mapOverviewResponse(response: CovidOverview?) = OverviewItem(
+        response?.confirmed?.value ?: 0,
+        response?.recovered?.value ?: 0,
+        response?.deaths?.value ?: 0)
+
+    private fun mapDailyResponse(responses: List<CovidDaily>): List<BaseViewItem> {
+        val items: MutableList<BaseViewItem> = mutableListOf()
+
+        for(response in responses) {
+            items.add(
+                DailyItem(
+                    response.objectid,
+                    response.deltaConfirmed,
+                    response.deltaRecovered,
+                    response.mainlandChina,
+                    response.otherLocations,
+                    response.reportDate,
+                    response.incrementRecovered,
+                    response.incrementConfirmed
+                )
+            )
+        }
+
+        return items.toList()
     }
-
-
 }
