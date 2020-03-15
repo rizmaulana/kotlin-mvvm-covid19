005d2d3364c77368e34cf0db9449ee96b00448b8
Pandu <pandu.baraja@airyrooms.com>
2020-03-15 17:17:53 +0700

    Create: new data mapper class covid pinned

--- app/src/main/java/id/rizmaulana/covid19/data/mapper/CovidDataMapper.kt
+++ app/src/main/java/id/rizmaulana/covid19/data/mapper/CovidDataMapper.kt
@@ -1,9 +1,11 @@
 package id.rizmaulana.covid19.data.mapper
 
 import id.rizmaulana.covid19.data.model.CovidDaily
+import id.rizmaulana.covid19.data.model.CovidDetail
 import id.rizmaulana.covid19.data.model.CovidOverview
 import id.rizmaulana.covid19.ui.adapter.viewholders.DailyItem
 import id.rizmaulana.covid19.ui.adapter.viewholders.OverviewItem
+import id.rizmaulana.covid19.ui.adapter.viewholders.PinnedItem
 
 object CovidDailyDataMapper {
 
@@ -23,8 +25,20 @@ object CovidDailyDataMapper {
 
 object CovidOverviewDataMapper {
 
-    fun transform(response: CovidOverview?) = OverviewItem(
-        response?.confirmed?.value ?: 0,
-        response?.recovered?.value ?: 0,
-        response?.deaths?.value ?: 0)
+    fun transform(response: CovidOverview?) = if(response != null) listOf(OverviewItem(
+        response.confirmed?.value ?: 0,
+        response.recovered?.value ?: 0,
+        response.deaths?.value ?: 0))
+    else emptyList()
+}
+
+object CovidPinnedDataMapper {
+
+    fun transform(response: CovidDetail?) = if(response != null) listOf(PinnedItem(
+        response.confirmed,
+        response.recovered,
+        response.deaths,
+        response.locationName,
+        response.lastUpdate))
+    else emptyList()
 }
