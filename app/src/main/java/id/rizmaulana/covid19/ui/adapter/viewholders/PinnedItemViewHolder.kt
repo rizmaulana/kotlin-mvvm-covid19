c820e65193b46a58f11112eb79f23ad5c8763caf
Pandu <pandu.baraja@airyrooms.com>
2020-03-15 17:16:08 +0700

    Create: new view holder pinned and tidying layout

---
+++ app/src/main/java/id/rizmaulana/covid19/ui/adapter/viewholders/PinnedItemViewHolder.kt
@@ -0,0 +1,41 @@
+package id.rizmaulana.covid19.ui.adapter.viewholders
+
+import android.view.View
+import id.rizmaulana.covid19.R
+import id.rizmaulana.covid19.databinding.ItemPinnedBinding
+import id.rizmaulana.covid19.ui.adapter.BaseViewHolder
+import id.rizmaulana.covid19.ui.base.BaseViewItem
+import id.rizmaulana.covid19.util.NumberUtils
+
+data class PinnedItem(
+    val confirmed: Int? = null,
+    val recovered: Int? = null,
+    val deaths: Int? = null,
+    val locationName: String,
+    val lastUpdate: Long
+): BaseViewItem {
+    override fun layoutResId(): Int = R.layout.item_pinned
+}
+
+class PinnedItemViewHolder(itemView: View) : BaseViewHolder<PinnedItem>(itemView) {
+    private val binding: ItemPinnedBinding = ItemPinnedBinding.bind(itemView)
+
+    override fun setOnClickListener(listener: (View) -> Unit) {
+        //Listener
+    }
+
+    override fun bind(item: PinnedItem) {
+        with(binding) {
+            val lastUpdate = NumberUtils.formatTime(item.lastUpdate)
+            txtLocation.text = item.locationName
+            txtUpdate.text = itemView.context.getString(R.string.information_last_update, lastUpdate)
+            txtData.text = "${item.confirmed ?: '-'}"
+            txtRcv.text = "${item.recovered ?: '-'}"
+            txtDeath.text = "${item.deaths ?: '-'}"
+        }
+    }
+
+    companion object {
+        const val LAYOUT = R.layout.item_pinned
+    }
+}
