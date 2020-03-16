6d81f86f8007906a6b1cbdca0fd2729d22b52252
Pandu <pandu.baraja@airyrooms.com>
2020-03-16 21:24:52 +0700

    Refactor: handle loading and error state, move all mapping in combineLatest callback

--- app/src/main/java/id/rizmaulana/covid19/ui/overview/DashboardViewModel.kt
+++ app/src/main/java/id/rizmaulana/covid19/ui/overview/DashboardViewModel.kt
@@ -5,10 +5,13 @@ import androidx.lifecycle.MutableLiveData
 import id.rizmaulana.covid19.data.mapper.CovidDailyDataMapper
 import id.rizmaulana.covid19.data.mapper.CovidOverviewDataMapper
 import id.rizmaulana.covid19.data.mapper.CovidPinnedDataMapper
+import id.rizmaulana.covid19.data.model.CovidDaily
+import id.rizmaulana.covid19.data.model.CovidDetail
+import id.rizmaulana.covid19.data.model.CovidOverview
 import id.rizmaulana.covid19.data.repository.Repository
-import id.rizmaulana.covid19.ui.adapter.viewholders.DailyItem
-import id.rizmaulana.covid19.ui.adapter.viewholders.OverviewItem
-import id.rizmaulana.covid19.ui.adapter.viewholders.PinnedItem
+import id.rizmaulana.covid19.data.repository.Result
+import id.rizmaulana.covid19.ui.adapter.viewholders.ErrorStateItem
+import id.rizmaulana.covid19.ui.adapter.viewholders.LoadingStateItem
 import id.rizmaulana.covid19.ui.adapter.viewholders.TextItem
 import id.rizmaulana.covid19.ui.base.BaseViewItem
 import id.rizmaulana.covid19.ui.base.BaseViewModel
@@ -39,49 +42,79 @@ class DashboardViewModel(
     val items: LiveData<List<BaseViewItem>>
         get() = _liveItems
 
+    private fun showLoadingState(){
+        _loading.value = true
+        if(_liveItems.value?.isEmpty() == null ||
+                _liveItems.value?.firstOrNull { it is ErrorStateItem } != null){
+            _liveItems.value = listOf(LoadingStateItem())
+        }
+    }
+
+    private fun showErrorState(throwable: Throwable){
+        _loading.value = false
+        if(_liveItems.value?.isEmpty() == null ||
+            _liveItems.value?.firstOrNull { it is ErrorStateItem || it is LoadingStateItem } != null){
+            _liveItems.value = listOf(handleThrowable(throwable))
+        }
+    }
+
     fun loadDashboard() {
+        showLoadingState()
+
         val overviewObservable = appRepository.overview()
             .observeOn(schedulerProvider.io()) //all stream below will be manage on io thread
-            .map { CovidOverviewDataMapper.transform(it) }
-            .switchIfEmpty(Observable.just(listOf())) //handle if observable empty
 
         val dailyObservable = appRepository.daily()
             .observeOn(schedulerProvider.io())
-            .map { CovidDailyDataMapper.transform(it) }
-            .switchIfEmpty(Observable.just(listOf()))
 
         val pinnedObservable = appRepository.getPinnedCountry()
             .observeOn(schedulerProvider.io())
-            .map { CovidPinnedDataMapper.transform(it) }
-            .switchIfEmpty(Observable.just(listOf()))
 
         Observable.combineLatest(
                 overviewObservable,
                 dailyObservable,
                 pinnedObservable,
-                Function3<List<OverviewItem>,
-                        List<DailyItem>,
-                        List<PinnedItem>,
-                        List<BaseViewItem>> { overviews, dailies, pinned ->
+                Function3<Result<CovidOverview>,
+                        Result<List<CovidDaily>>,
+                        Result<CovidDetail>,
+                        Pair<List<BaseViewItem>, Throwable?>> { overview, daily, pinned ->
 
                     val items: MutableList<BaseViewItem> = mutableListOf()
+                    var currentThrowable: Throwable? = null
 
-                    //GENERATE SCREEN POSITION HERE
-                    if(overviews.isNotEmpty()) items.addAll(overviews)
-                    if(pinned.isNotEmpty()) items.addAll(pinned)
-
-                    if(dailies.isNotEmpty()) {
-                        items.add(TextItem("Daily Updates"))
-                        items.addAll(dailies)
+                    with(overview){
+                        items.add(CovidOverviewDataMapper.transform(data))
+                        error?.let { currentThrowable = it }
                     }
 
-                    return@Function3 items.toList()
+                    with(pinned){
+                        CovidPinnedDataMapper.transform(data)?.let {
+                            items.add(it)
+                        }
+                        error?.let { currentThrowable = it }
+                    }
+
+                    with(daily){
+                        val dailies = CovidDailyDataMapper.transform(data)
+                        if(dailies.isNotEmpty()) {
+                            items.add(TextItem("Daily Updates"))
+                            items.addAll(dailies)
+                        }
+                        error?.let { currentThrowable = it }
+                    }
+
+                    return@Function3 items.toList() to currentThrowable
                 })
         .observeOn(schedulerProvider.ui()) //go back to ui thread
-        .subscribe({
-            _liveItems.postValue(it)
+        .subscribe({ (result, throwable) ->
+            _liveItems.postValue(result)
+
+            //For now only check if there is a throwable
+            if(throwable != null) _toastMessage.value = Constant.ERROR_MESSAGE
+            _loading.value = false
         }, {
-            _toastMessage.postValue(Constant.ERROR_MESSAGE)
+            _toastMessage.value = Constant.ERROR_MESSAGE
+            showErrorState(it)
         }).addTo(compositeDisposable)
     }
 }
