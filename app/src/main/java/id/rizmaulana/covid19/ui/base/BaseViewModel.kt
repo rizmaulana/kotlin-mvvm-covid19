cfb0625094fdf611446b4a925c1d396caa12efd8
Pandu <pandu.baraja@airyrooms.com>
2020-03-16 21:22:24 +0700

    Add: create handle throwable that return corresponding state info

--- app/src/main/java/id/rizmaulana/covid19/ui/base/BaseViewModel.kt
+++ app/src/main/java/id/rizmaulana/covid19/ui/base/BaseViewModel.kt
@@ -2,7 +2,12 @@ package id.rizmaulana.covid19.ui.base
 
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.ViewModel
+import id.rizmaulana.covid19.R
+import id.rizmaulana.covid19.ui.adapter.viewholders.ErrorStateItem
 import io.reactivex.disposables.CompositeDisposable
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 /**
  * rizmaulana 2020-02-24.
@@ -18,5 +23,16 @@ abstract class BaseViewModel : ViewModel(), LifecycleObserver {
             dispose()
         }
         super.onCleared()
+    }
+
+    fun handleThrowable(throwable: Throwable): BaseViewItem {
+        return when(throwable) {
+            is ConnectException,
+            is SocketTimeoutException,
+            is UnknownHostException -> {
+                ErrorStateItem(R.string.connection_error_title, R.string.connection_error_description)
+            }
+            else -> ErrorStateItem(R.string.general_error_title, R.string.general_error_description)
+        }
     }
 }
