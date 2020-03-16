77d4bc88d07bfc5b01b1bcba9b1c2274d7bdfec3
Pandu <pandu.baraja@airyrooms.com>
2020-03-16 21:20:57 +0700

    Refactor: rename ViewHolder and bind title and subtitle

---
+++ app/src/main/java/id/rizmaulana/covid19/ui/adapter/viewholders/ErrorStateItemViewHolder.kt
@@ -0,0 +1,36 @@
+package id.rizmaulana.covid19.ui.adapter.viewholders
+
+import android.view.View
+import id.rizmaulana.covid19.R
+import id.rizmaulana.covid19.databinding.ItemErrorStateBinding
+import id.rizmaulana.covid19.ui.adapter.BaseViewHolder
+import id.rizmaulana.covid19.ui.base.BaseViewItem
+
+
+class ErrorStateItem(
+    val titleResId: Int,
+    val subtitleResId: Int
+): BaseViewItem {
+    override fun layoutResId(): Int = R.layout.item_error_state
+}
+
+class ConnectionErrorStateItemViewHolder(itemView: View) : BaseViewHolder<ErrorStateItem>(itemView) {
+
+    private val binding: ItemErrorStateBinding = ItemErrorStateBinding.bind(itemView)
+
+    override fun setOnClickListener(listener: (View) -> Unit) {
+        //Listener
+        binding.textRetry.setOnClickListener { listener.invoke(it) }
+    }
+
+    override fun bind(item: ErrorStateItem) {
+        with(binding){
+            textTitle.text = itemView.context.getString(item.titleResId)
+            textSubtitle.text = itemView.context.getString(item.subtitleResId)
+        }
+    }
+
+    companion object {
+        const val LAYOUT = R.layout.item_error_state
+    }
+}
