2ad63156caf6bceb56b5b1b2fb9ba0850bb05908
rizmaulana <rizmaulana@live.com>
2020-03-22 11:34:53 +0700

    Add generated source

---
+++ app/src/main/java/id/rizmaulana/covid19/data/source/generated/AppGeneratedSource.kt
@@ -0,0 +1,8 @@
+package id.rizmaulana.covid19.data.source.generated
+
+/**
+ * rizmaulana 22/03/20.
+ */
+class AppGeneratedSource {
+
+}
--- app/src/main/java/id/rizmaulana/covid19/di/PersistenceModule.kt
+++ app/src/main/java/id/rizmaulana/covid19/di/PersistenceModule.kt
@@ -1,5 +1,6 @@
 package id.rizmaulana.covid19.di
 
+import id.rizmaulana.covid19.data.source.generated.AppGeneratedSource
 import id.rizmaulana.covid19.data.source.pref.AppPrefSource
 import org.koin.dsl.module
 
@@ -11,5 +12,10 @@ val persistenceModule = module {
     single {
         AppPrefSource()
     }
+
+    single {
+        AppGeneratedSource()
+    }
 }
+
 
