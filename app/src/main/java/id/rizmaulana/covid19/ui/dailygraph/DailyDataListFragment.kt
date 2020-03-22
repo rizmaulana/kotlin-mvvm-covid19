43caa9d887ca57665997f8688f81e97609316bce
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-03-22 11:15:06 +0700

    Add fragments

---
+++ app/src/main/java/id/rizmaulana/covid19/ui/dailygraph/DailyDataListFragment.kt
@@ -0,0 +1,11 @@
+package id.rizmaulana.covid19.ui.dailygraph
+
+import id.rizmaulana.covid19.ui.base.BaseFragment
+
+class DailyDataListFragment: BaseFragment() {
+
+    override fun observeChange() {
+
+    }
+
+}
---
+++ app/src/main/java/id/rizmaulana/covid19/ui/dailygraph/DailyGraphFragment.kt
@@ -0,0 +1,10 @@
+package id.rizmaulana.covid19.ui.dailygraph
+
+import id.rizmaulana.covid19.ui.base.BaseFragment
+
+class DailyGraphFragment: BaseFragment() {
+
+    override fun observeChange() {
+
+    }
+}
