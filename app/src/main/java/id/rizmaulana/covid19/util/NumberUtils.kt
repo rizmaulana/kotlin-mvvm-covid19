7646b46e796a2f50e728e0fba2be0748dbfbd4b4
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-03-25 13:02:06 +0700

    Reformat short format from string

--- app/src/main/java/id/rizmaulana/covid19/util/NumberUtils.kt
+++ app/src/main/java/id/rizmaulana/covid19/util/NumberUtils.kt
@@ -7,13 +7,14 @@ import java.util.*
 /**
  * rizmaulana 04/03/20.
  */
-object NumberUtils{
+object NumberUtils {
 
-    fun numberFormat(number: Int) = NumberFormat.getNumberInstance(Locale.getDefault()).format(number)
+    fun numberFormat(number: Int) =
+        NumberFormat.getNumberInstance(Locale.getDefault()).format(number)
 
     fun extractDigit(number: String) = Regex("[^0-9]").replace(number, "").toInt()
 
-    fun formatTime(time: Long) : String{
+    fun formatTime(time: Long): String {
         val sdf = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
         return sdf.format(Date(time))
     }
@@ -21,6 +22,13 @@ object NumberUtils{
     fun formatShortDate(time: Long): String {
         val sdf = SimpleDateFormat("dd MMM", Locale.getDefault())
         return sdf.format(Date(time))
+    }
+
+    fun formatShortDate(time: String): String {
+        val parser = SimpleDateFormat("yyyy-mm-dd", Locale.getDefault())
+        val origin = parser.parse(time)
+        val sdf = SimpleDateFormat("dd MMM", Locale.getDefault())
+        return sdf.format(origin)
     }
 
 }
