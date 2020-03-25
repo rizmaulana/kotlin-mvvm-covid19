03bc9dc2989161d542799076e34cbb2a4698265d
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-03-25 13:15:32 +0700

    Complete daily compact view holder

--- app/src/main/java/id/rizmaulana/covid19/ui/adapter/viewholders/DailyCompactViewHolder.kt
+++ app/src/main/java/id/rizmaulana/covid19/ui/adapter/viewholders/DailyCompactViewHolder.kt
@@ -1,13 +1,35 @@
 package id.rizmaulana.covid19.ui.adapter.viewholders
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import id.rizmaulana.covid19.R
+import id.rizmaulana.covid19.databinding.ItemDailyCompactBinding
 import id.rizmaulana.covid19.ui.adapter.BaseViewHolder
+import id.rizmaulana.covid19.util.IncrementStatus
+import id.rizmaulana.covid19.util.NumberUtils
 
 class DailyCompactViewHolder(itemView: View) : BaseViewHolder<DailyItem>(itemView) {
 
-    override fun bind(item: DailyItem) {
+    private val binding = ItemDailyCompactBinding.bind(itemView)
 
+    override fun bind(item: DailyItem) {
+        with(binding) {
+            txtDate.text = item.reportDate
+
+            root.context?.let {
+                txtConfirmed.text = it.getString(R.string.confirmed_case_count, NumberUtils.numberFormat(item.deltaConfirmed))
+                txtRecovered.text = it.getString(R.string.recovered_case_count, NumberUtils.numberFormat(item.deltaRecovered))
+
+                imgRecovered.setImageDrawable(ContextCompat.getDrawable(it,getFluctuationIcon(item.incrementRecovered)))
+                imgConfirmed.setImageDrawable(ContextCompat.getDrawable(it, getFluctuationIcon(item.incrementConfirmed)))
+            }
+        }
+    }
+
+    private fun getFluctuationIcon(status: Int) = when (status) {
+        IncrementStatus.INCREASE -> R.drawable.ic_trending_up
+        IncrementStatus.DECREASE -> R.drawable.ic_trending_down
+        else -> R.drawable.ic_trending_flat
     }
 
     override fun setOnClickListener(listener: (View) -> Unit) {
